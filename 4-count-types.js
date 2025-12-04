f14fe9595fb67e33fd45ff4ea5e3f75074c20fc6
Follish <90408443+Cheburek534@users.noreply.github.com>
2025-12-04 21:12:59 +0200

    Update 4-count-types.js

--- 4-count-types.js
+++ 4-count-types.js
@@ -1,21 +1,18 @@
-
 const array = [true, 'Hello World', 8, 152, -20, false, false, 'TikTok', 36.7, 'Dota', 0, 'Python'];
 console.log('Array:', array);
-let types = {number: 0, string: 0, boolean: 0, object: 0, symbol: 0};
-
+let types = {};
 console.log('Initial element:', types);
 for (const item of array) {
     const type = typeof item;
+    if (!types[type]) {
+        types[type] = 0;
+    }
     types[type]++;
 }
-
-console.log(Result:', types);
+console.log('Result:', types);
 console.log('Changed example');
-
 let dynamicTypes = {};
-
 console.log('Hollow object:', dynamicTypes);
-
 for (const item of array) {
     const type = typeof item;
     
